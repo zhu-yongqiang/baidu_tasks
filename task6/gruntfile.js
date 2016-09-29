b834463c39e9d4f83d74a0f1caaa17854d3bda72
zhu-yongqiang <1225581453@qq.com>
2016-09-29 12:37:46 +0800

    add6

---
+++ task6/gruntfile.js
@@ -0,0 +1,29 @@
+//wrapper函数
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/main.css': 'css/main.scss'
+                },
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'css/main.scss'
+                ],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-sass'); 
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['sass']);
+};
